3e72fc4cd49d99f2bad4e88d58bada36cafcea18
Luc IJspeert <46969344+LucIJspeert@users.noreply.github.com>
2025-07-16 12:26:09 +0200

    update run_first_use.py 2

--- star_shine/api/run_first_use.py
+++ star_shine/api/run_first_use.py
@@ -13,22 +13,26 @@ import importlib.resources
 import star_shine as sts
 
 
-# get the path to the test light curve
-data_path_traversable = importlib.resources.files('star_shine.data')
-target_id = 'sim_000_lc'
-file = data_path_traversable.joinpath(target_id + '.dat').as_posix()
-data_path = os.path.split(file)[0]
-file_list = [file]
+def first_use_script():
+    # get the path to the test light curve
+    data_path_traversable = importlib.resources.files('star_shine.data')
+    target_id = 'sim_000_lc'
+    file = data_path_traversable.joinpath(target_id + '.dat').as_posix()
+    data_path = os.path.split(file)[0]
+    file_list = [file]
 
-# initialise the data and pipeline
-data = sts.Data.load_data(file_list, data_dir='', target_id=target_id, data_id='', logger=None)
-pipeline = sts.Pipeline(data, save_dir=data_path, logger=None)
+    # initialise the data and pipeline
+    data = sts.Data.load_data(file_list, data_dir='', target_id=target_id, data_id='', logger=None)
+    pipeline = sts.Pipeline(data, save_dir=data_path, logger=None)
 
-# do a first run without fitting every step and lower group size
-sts.update_config(settings={'optimise_step': False, 'min_group': 15, 'max_group': 20})
-pipeline.run()
+    # do a first run without fitting every step and lower group size
+    sts.update_config(settings={'optimise_step': False, 'min_group': 15, 'max_group': 20})
+    pipeline.run()
 
-# do a second run with fitting every step
-sts.update_config(settings={'optimise_step': True})
-pipeline.run()
+    # do a second run with fitting every step
+    sts.update_config(settings={'optimise_step': True})
+    pipeline.run()
 
+
+if __name__ == '__main__':
+    first_use_script()
